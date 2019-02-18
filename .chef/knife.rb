e8c732c7ec764c8e7da0b713db9b571ca1c889d9
valbeat <kj1ktk@gmail.com>
2019-02-18 21:25:29 +0900

    Remove unused code

--- .chef/knife.rb
+++
@@ -1,8 +0,0 @@
-log_level                :info
-log_location             STDOUT
-node_name                'kajitack'
-client_key               '/Users/kajikawa/.chef/kajitack.pem'
-validation_client_name   'chef-validator'
-validation_key           '/etc/chef-server/chef-validator.pem'
-chef_server_url          'https://valbeat.local:443'
-syntax_check_cache_path  '/Users/kajikawa/.chef/syntax_check_cache'
